9661e4c839ff65d4332605d9d712924583f4e192
rafael <rafaelmoreiracvo@yahoo.com.br>
2020-03-18 07:58:25 -0300

    auto arima

--- curso_udemy/auto_arima.R
+++ curso_udemy/auto_arima.R
@@ -13,3 +13,15 @@ modelo2
 # Best model: ARIMA(0,1,3)(0,1,1)[12]  
 # sigma^2 estimated as 0.08518:  log likelihood=-83.43
 # AIC=176.86   AICc=177   BIC=197.47
+
+prev1 <- forecast(modelo, h=12)
+autoplot(prev1)
+
+prev2 <- forecast(modelo2, h=12)
+autoplot(prev2)
+
+prev1$mean
+prev2$mean
+
+plot(prev1)
+lines(prev2$mean, col='red')
